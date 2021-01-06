5cf8e0c4aa063861901a67b2e63f0e19eebb4382
bookert921 <bookert921@gmail.com>
2021-01-06 15:48:06 -0800

    maybe fixes blank screen?

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
   return (
-    <Router basename={window.location.pathname || 'bookNbeyond'} className="App">
+    <Router basename={window.location.pathname || 'bookert921.github.io/bookNbeyond'} className="App">
       <Home />
     </Router>
   );
