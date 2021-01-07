7e7b6f88572eb936ff1f60b2293afd398955b8e7
bookert921 <bookert921@gmail.com>
2021-01-06 20:42:40 -0800

    need to fix display issues in safari browser

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
   return (
-    <Router basename={window.location.pathname || 'booknbeyond'} className="App">
+    <Router className="App">
       <Home />
     </Router>
   );
